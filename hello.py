30b3a55bb8462ef83a06490695ccdf38543640fe
awsmdavid <awsmdavid@gmail.com>
2016-10-05 21:09:32 -0400

    fixed templating and updated time logic

--- hello.py
+++ hello.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
 import time
 import calendar
+import datetime
 
 app = Flask(__name__)
 
+# epoch = datetime.datetime.utcfromtimestamp(0)
+
 @app.route('/')
 def index():
-    millimeter = get_time()
-    return render_template('index.html', time=millimeter)
+    milliseconds = get_time()
+    return render_template('index.html', time=milliseconds)
 
 def get_time():
-    seconds = calendar.timegm(time.gmtime())
-    return seconds
+    milli = time.time() * 1000
+    unsci_milli = '{:.0f}'.format(milli)
+    return unsci_milli
+
+# def get_time():
+#     return (time.time() - epoch).total_seconds() * 1000.0
